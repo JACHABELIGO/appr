07f7b8c8ebf6226c8d66ed1449a1e67f14f658ea
Gideon Okuro <gideonollonde@gmail.com>
2020-11-23 17:23:24 +0300

    initial commit

---
+++ config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  siteTitle: 'APPR', // <title>
+  manifestName: 'Spectral',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/Appr/`, // This path is subpath of your hosting https://domain/portfolio
+  heading: 'APPR',
+  subHeading: 'We Love Our Users',
+  // social
+  socialLinks: [
+    {
+      style: 'brands',
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/anubhavsrivastava',
+    },
+    {
+      style: 'brands',
+      icon: 'fa-twitter',
+      name: 'Twitter',
+      url: 'https://twitter.com/onlyanubhav',
+    },
+    {
+      style: 'solid',
+      icon: 'fa-envelope',
+      name: 'Email',
+      url: 'mailto:test@example.com',
+    },
+  ],
+};
---
+++ src/pages/index.js
@@ -0,0 +1,150 @@
+import React from 'react';
+
+import Layout from '../components/Layout';
+
+import Scroll from '../components/Scroll';
+
+import config from '../../config';
+const IndexPage = () => (
+  <Layout>
+    <section id="banner">
+      <div className="inner">
+        <h2>{config.heading}</h2>
+        <p>{config.subHeading}</p>
+        <ul className="actions special">
+          <li>
+            <Scroll type="id" element="one">
+              <a href="/#" className="button primary">
+                Explore
+              </a>
+            </Scroll>
+          </li>
+        </ul>
+      </div>
+      <Scroll type="id" element="one">
+        <a href="#one" className="more">
+          Learn More
+        </a>
+      </Scroll>
+    </section>
+
+    <section id="one" className="wrapper style1 special">
+      <div className="inner">
+        <header className="major">
+          <h2>
+            WHO WE ARE
+          </h2>
+          <p>
+            Aliquam ut ex ut augue consectetur interdum. Donec amet imperdiet
+            eleifend
+            <br />
+            fringilla tincidunt. Nullam dui leo Aenean mi ligula, rhoncus
+            ullamcorper.
+          </p>
+        </header>
+        <ul className="icons major">
+          <li>
+            <span className="icon fa-gem major style1">
+              <span className="label">Lorem</span>
+            </span>
+          </li>
+          <li>
+            <span className="icon fa-heart major style2">
+              <span className="label">Ipsum</span>
+            </span>
+          </li>
+          <li>
+            <span className="icon solid fa-code major style3">
+              <span className="label">Dolor</span>
+            </span>
+          </li>
+        </ul>
+      </div>
+    </section>
+
+    <section id="three" className="wrapper style3 special">
+      <div className="inner">
+        <header className="major">
+          <h2>SERVICES</h2>
+          <p>
+            Aliquam ut ex ut augue consectetur interdum. Donec amet imperdiet
+            eleifend
+            <br />
+            fringilla tincidunt. Nullam dui leo Aenean mi ligula, rhoncus
+            ullamcorper.
+          </p>
+        </header>
+        <ul className="features">
+          <li className="icon fa-paper-plane">
+            <h3>MOBILE DEVELOPMENT</h3>
+            <p>
+              Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem
+              tincidunt nullam amet leo Aenean ligula consequat consequat.
+            </p>
+          </li>
+          <li className="icon solid fa-laptop">
+            <h3>WEB DEVELOPMENT</h3>
+            <p>
+              Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem
+              tincidunt nullam amet leo Aenean ligula consequat consequat.
+            </p>
+          </li>
+          <li className="icon solid fa-code">
+            <h3>IT CONSULTING</h3>
+            <p>
+              Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem
+              tincidunt nullam amet leo Aenean ligula consequat consequat.
+            </p>
+          </li>
+          <li className="icon solid fa-headphones-alt">
+            <h3>Mauris Imperdiet</h3>
+            <p>
+              Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem
+              tincidunt nullam amet leo Aenean ligula consequat consequat.
+            </p>
+          </li>
+          <li className="icon fa-heart">
+            <h3>Aenean Primis</h3>
+            <p>
+              Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem
+              tincidunt nullam amet leo Aenean ligula consequat consequat.
+            </p>
+          </li>
+          <li className="icon fa-flag">
+            <h3>Tortor Ut</h3>
+            <p>
+              Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem
+              tincidunt nullam amet leo Aenean ligula consequat consequat.
+            </p>
+          </li>
+        </ul>
+      </div>
+    </section>
+
+    <section id="cta" className="wrapper style4">
+      <div className="inner">
+        <header>
+          <h2>Arcue ut vel commodo</h2>
+          <p>
+            Aliquam ut ex ut augue consectetur interdum endrerit imperdiet amet
+            eleifend fringilla.
+          </p>
+        </header>
+        <ul className="actions stacked">
+          <li>
+            <a href="/#" className="button fit primary">
+              Activate
+            </a>
+          </li>
+          <li>
+            <a href="/#" className="button fit">
+              Learn More
+            </a>
+          </li>
+        </ul>
+      </div>
+    </section>
+  </Layout>
+);
+
+export default IndexPage;
